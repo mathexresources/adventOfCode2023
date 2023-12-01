de92ce6621ca2b6784642f4b0f6fadbdc3caba8e
mathexresources <matous.drabek@student.ossp.cz>
2023-12-01 13:30:37 +0100

    sync

---
+++ day1/p2.rb
@@ -0,0 +1,15 @@
+class DayOne2023 def self.part_one(lines) lines.sum do |line| digits = line.scan(/\d/) first = digits.first last = digits.last "#{first}#{last}".to_i end end
+
+    def self.part_two(lines) numbers = %w[one two three four five six seven eight nine] lines.sum do |line| digits = line.scan(/(?=(one|two|three|four|five|six|seven|eight|nine|\d))/).flatten first = numbers.index(digits.first)&.next || digits.first last = numbers.index(digits.last)&.next || digits.last "#{first}#{last}".to_i end end end
+    
+    def self.part_one(lines) lines.sum do |line| digits = line.scan(/\d/) first = digits.first last = digits.last "#{first}#{last}".to_i end end
+    
+    def self.part_two(lines) numbers = %w[one two three four five six seven eight nine] lines.sum do |line| digits = line.scan(/(?=(one|two|three|four|five|six|seven|eight|nine|\d))/).flatten first = numbers.index(digits.first)&.next || digits.first last = numbers.index(digits.last)&.next || digits.last "#{first}#{last}".to_i end end end
+    
+    class DayOne2023 def self.part_one(lines) lines.sum do |line| digits = line.scan(/\d/) first = digits.first last = digits.last "#{first}#{last}".to_i end end
+    
+    def self.part_two(lines) numbers = %w[one two three four five six seven eight nine] lines.sum do |line| digits = line.scan(/(?=(one|two|three|four|five|six|seven|eight|nine|\d))/).flatten first = numbers.index(digits.first)&.next || digits.first last = numbers.index(digits.last)&.next || digits.last "#{first}#{last}".to_i end end end
+    
+    class DayOne2023 def self.part_one(lines) lines.sum do |line| digits = line.scan(/\d/) first = digits.first last = digits.last "#{first}#{last}".to_i end end
+    
+    def self.part_two(lines) numbers = %w[one two three four five six seven eight nine] lines.sum do |line| digits = line.scan(/(?=(one|two|three|four|five|six|seven|eight|nine|\d))/).flatten first = numbers.index(digits.first)&.next || digits.first last = numbers.index(digits.last)&.next || digits.last "#{first}#{last}".to_i end end end 
